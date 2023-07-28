c5101e087d27d24153232266b3634308e42ada47
mmateos <mmateos@hiberus.com>
2023-07-28 09:06:42 +0200

    Upgrading to AGP 8.0.0

--- Plugins/dependencies/src/main/kotlin/com/marmatsan/dependencies/Dependency.kt
+++
@@ -1,7 +0,0 @@
-package com.marmatsan.dependencies
-
-abstract class Dependency(
-    open val name: String,
-    open val group: String,
-    open val version: String
-)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    includeBuild("./Plugins")
+    // includeBuild("./plugins")
 }
 
 plugins {
